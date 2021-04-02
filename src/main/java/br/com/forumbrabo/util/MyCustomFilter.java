eeec96b85a0ef64305dad5e9520c800b9faaa7fd
Mendoka <80711932+Mendoka@users.noreply.github.com>
2021-04-02 13:02:41 -0300

    Add files via upload

---
+++ src/main/java/br/com/forumbrabo/util/MyCustomFilter.java
@@ -0,0 +1,27 @@
+package br.com.forumbrabo.util;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpFilter;
+
+public class MyCustomFilter extends HttpFilter {
+
+    private static final long serialVersionUID = -1L;
+    private static Logger log = Logger.getLogger(MyCustomFilter.class.getName());
+
+    public MyCustomFilter() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        super.doFilter(req, res, chain);
+        log.info("filter Request");
+    }
+
+}
